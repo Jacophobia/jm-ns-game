5941a4aed9b4d4915ea114c6f23253160169f14c
Jacob <74572036+Jacophobia@users.noreply.github.com>
2023-12-04 13:03:11 -0700

    Update Collision.cs

--- MonoGame/Decorators/Collision.cs
+++ MonoGame/Decorators/Collision.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using MonoGame.Entities;
@@ -7,56 +7,85 @@ using MonoGame.Interfaces;
 
 namespace MonoGame.Decorators;
 
-// ReSharper disable once ClassNeverInstantiated.Global
 public class Collision : EntityDecorator
 {
     public Collision(Entity @base) : base(@base)
     {
-        // no new behavior to add
+        // No new behavior to add
     }
 
-    private static bool AreMovingTowardsEachOther(Vector2 position1, Vector2 velocity1, Vector2 position2,
-        Vector2 velocity2)
+    private static bool AreMovingTowardsEachOther(Vector2 position1, Vector2 velocity1, Vector2 position2, Vector2 velocity2)
     {
-        // Calculate position differences
         var deltaPosition = position2 - position1;
-
-        // Calculate velocity differences
         var deltaVelocity = velocity2 - velocity1;
-
-        // Calculate the dot product
-        var dotProduct = Vector2.Dot(deltaPosition, deltaVelocity * 100);
-
-        // If the dot product is negative, objects are moving towards each other
-        return dotProduct < 0;
+        return Vector2.Dot(deltaPosition, deltaVelocity) < 0;
     }
 
     protected override bool IsCollidingWith(ICollidable rhs, float deltaTime, out Rectangle? overlap)
     {
+        overlap = null;
+
         if (IsStatic && rhs.IsStatic)
         {
-            overlap = null;
             return false;
         }
 
-        var lhsBounds = Bounds;
-        var rhsBounds = rhs.Bounds;
+        var relativeVelocity = Velocity - rhs.Velocity;
 
-        lhsBounds.Location += (Velocity * deltaTime).ToPoint();
-        rhsBounds.Location += (rhs.Velocity * deltaTime).ToPoint();
-        
-        // Find the intersection rectangle
-        overlap = Rectangle.Intersect(lhsBounds, rhsBounds);
+        if (GetCollisionTimes(Bounds, rhs.Bounds, relativeVelocity, deltaTime, out float enterTime, out float exitTime, out Vector2 normal))
+        {
+            var collisionPointLhs = Base.Position + Velocity * enterTime;
+            var collisionPointRhs = rhs.Position + rhs.Velocity * enterTime;
 
-        // TODO: there is an issue where if something gets completely enveloped into something else between frames, it freezes the game
-        // TODO: we also need to incorporate the Source rectangle in this so that collisions are calculated correctly when a sprite sheet is used
-        var collisionLocation = overlap.Value.Center.ToVector2();
-        
-        var reverseLhsNormal = -CalculateCollisionNormal(rhs, collisionLocation);
-        
-        return overlap is not { IsEmpty: true }
-                && AreMovingTowardsEachOther(lhsBounds.Center.ToVector2(), Velocity, collisionLocation, rhs.Velocity)
-                && CollisionData.Collides(lhsBounds, rhs.CollisionData, rhsBounds, overlap.Value);
+            var lhsBoundsAtCollision = new Rectangle(collisionPointLhs.ToPoint(), Bounds.Size);
+            var rhsBoundsAtCollision = new Rectangle(collisionPointRhs.ToPoint(), rhs.Bounds.Size);
+
+            overlap = Rectangle.Intersect(lhsBoundsAtCollision, rhsBoundsAtCollision);
+
+            return overlap != null && !overlap.IsEmpty && CollisionData.Collides(lhsBoundsAtCollision, rhs.CollisionData, rhsBoundsAtCollision, overlap);
+        }
+
+        return false;
+    }
+
+    private bool GetCollisionTimes(Rectangle lhsBounds, Rectangle rhsBounds, Vector2 velocity, float deltaTime, out float enterTime, out float exitTime, out Vector2 normal)
+    {
+        // Get times for X and Y axes
+        var (enterX, exitX) = GetCollisionTimes1D(lhsBounds.X, rhsBounds.X, lhsBounds.Width, rhsBounds.Width, velocity.X, 0, deltaTime);
+        var (enterY, exitY) = GetCollisionTimes1D(lhsBounds.Y, rhsBounds.Y, lhsBounds.Height, rhsBounds.Height, velocity.Y, 0, deltaTime);
+
+        // Find the earliest/latest times of collision/leaving
+        enterTime = Math.Max(enterX, enterY);
+        exitTime = Math.Min(exitX, exitY);
+
+        // Determine the collision normal
+        normal = enterX > enterY ? new Vector2(velocity.X < 0 ? 1 : -1, 0) : new Vector2(0, velocity.Y < 0 ? 1 : -1);
+
+        return enterTime < exitTime && enterTime >= 0 && enterTime < deltaTime;
+    }
+
+    private (float enter, float exit) GetCollisionTimes1D(float posA, float posB, float sizeA, float sizeB, float velA, float velB, float deltaTime)
+    {
+        float enterTime, exitTime;
+
+        if (velA - velB == 0) // Parallel movement or static
+        {
+            enterTime = float.NegativeInfinity;
+            exitTime = float.PositiveInfinity;
+        }
+        else
+        {
+            var relativePos = posB - posA;
+            enterTime = (relativePos) / (velA - velB);
+            exitTime = (relativePos + sizeB - sizeA) / (velA - velB);
+        }
+
+        if (enterTime > exitTime)
+        {
+            (enterTime, exitTime) = (exitTime, enterTime); // Swap if needed
+        }
+
+        return (enterTime, exitTime);
     }
 
     protected override void OnHandleCollisionWith(ICollidable rhs, float deltaTime, Rectangle? overlap)
@@ -65,74 +94,34 @@ public class Collision : EntityDecorator
         Debug.Assert(rhs != null);
         Debug.Assert(overlap != null);
 
-        var initialMagnitude = (Velocity + rhs.Velocity).Length();
-
-        var lhsVelocity = Velocity;
-        var rhsVelocity = rhs.Velocity;
-        
         var collisionCoordinate = overlap.Value.Center.ToVector2();
+        var collisionNormal = CalculateCollisionNormal(rhs, collisionCoordinate);
+        var inverseCollisionNormal = -rhs.CalculateCollisionNormal(this, collisionCoordinate);
 
-        // Calculate the normal (n) and tangential (t) direction vectors
-        var lhsNormal = CalculateCollisionNormal(rhs, collisionCoordinate);
-        var rhsNormal = -rhs.CalculateCollisionNormal(this, collisionCoordinate);
+        var relativeVelocity = Velocity - rhs.Velocity;
+        var velocityAlongNormal = Vector2.Dot(relativeVelocity, collisionNormal);
+
+        if (velocityAlongNormal > 0) return;
+
+        var e = Math.Min(RestitutionCoefficient, rhs.RestitutionCoefficient);
+        var j = -(1 + e) * velocityAlongNormal;
+        j /= 1 / Mass + 1 / rhs.Mass;
+
+        var impulse = j * collisionNormal;
         
-        // Decompose velocities into normal and tangential components
-        var v1N = lhsVelocity.X * lhsNormal.X + lhsVelocity.Y * lhsNormal.Y; // Dot product
-        var v1T = -lhsVelocity.X * lhsNormal.Y + lhsVelocity.Y * lhsNormal.X; // Perpendicular dot product
+        const float percent = 0.2f; // Penetration percentage to correct
+        const float slop = 0.01f; // Allowable penetration
+        var correction = Math.Max(overlap.Value.Height - slop, 0.0f) / (1 / Mass + 1 / rhs.Mass) * percent * collisionNormal;
 
-        var overlapMass = overlap.Value.Mass();
-
-        var lhsRestitution = Math.Min(overlapMass / Mass, RestitutionCoefficient);
-        var rhsRestitution = Math.Min(overlapMass / rhs.Mass, rhs.RestitutionCoefficient);
-
-        if (IsStatic)
+        if (!IsStatic)
         {
-            // If this object is static, only adjust the other object's velocity
-            var newV2N = -rhsRestitution *
-                         (rhsVelocity.X * rhsNormal.X + rhsVelocity.Y * rhsNormal.Y);
-            rhsVelocity.X = newV2N * rhsNormal.X - (-rhsVelocity.X * rhsNormal.Y + rhsVelocity.Y * rhsNormal.X) * rhsNormal.Y;
-            rhsVelocity.Y = newV2N * rhsNormal.Y + (-rhsVelocity.X * rhsNormal.Y + rhsVelocity.Y * rhsNormal.X) * rhsNormal.X;
+            Position -= correction / Mass; // TODO: Fix the spatial partition so it allows us to separate the objects on collision
+            Velocity -= impulse / Mass;
         }
-        else if (rhs.IsStatic)
+        if (!rhs.IsStatic)
         {
-            // Collision with a static object
-            var newV1N = -lhsRestitution * v1N;
-
-            // Recompose velocity for the dynamic object
-            lhsVelocity.X = newV1N * lhsNormal.X - v1T * lhsNormal.Y;
-            lhsVelocity.Y = newV1N * lhsNormal.Y + v1T * lhsNormal.X;
+            rhs.Position += correction / rhs.Mass;
+            rhs.Velocity += impulse / rhs.Mass;
         }
-        else
-        {
-            // Collision with another dynamic object
-            var v2N = rhsVelocity.X * rhsNormal.X + rhsVelocity.Y * rhsNormal.Y;
-            var v2T = -rhsVelocity.X * rhsNormal.Y + rhsVelocity.Y * rhsNormal.X;
-
-            // Apply the restitution coefficient
-            var combinedRestitution = (lhsRestitution + rhsRestitution) / 2f;
-
-            // Exchange normal components in an inelastic collision
-            var newV1N = combinedRestitution * (v1N * (Mass - rhs.Mass) + 2f * rhs.Mass * v2N) /
-                         (Mass + rhs.Mass);
-            var newV2N = combinedRestitution * (v2N * (rhs.Mass - Mass) + 2f * Mass * v1N) /
-                         (Mass + rhs.Mass);
-
-            // Recompose velocities for both objects
-            lhsVelocity.X = newV1N * lhsNormal.X - v1T * lhsNormal.Y;
-            lhsVelocity.Y = newV1N * lhsNormal.Y + v1T * lhsNormal.X;
-            rhsVelocity.X = newV2N * rhsNormal.X - v2T * rhsNormal.Y;
-            rhsVelocity.Y = newV2N * rhsNormal.Y + v2T * rhsNormal.X;
-        }
-
-        var currentMagnitude = (lhsVelocity + rhsVelocity).Length();
-        
-        if (initialMagnitude != 0 && currentMagnitude >= initialMagnitude)
-        {
-            lhsVelocity *= initialMagnitude / currentMagnitude;
-            rhsVelocity *= initialMagnitude / currentMagnitude;
-        }
-
-        Velocity = lhsVelocity;
-        rhs.Velocity = rhsVelocity;
     }
 }
